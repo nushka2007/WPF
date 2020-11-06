f8b1a066afc8d83cec38b77a8b5a744508e5760c
nushka2007 <73809343+nushka2007@users.noreply.github.com>
2020-11-07 07:06:09 +0800

    Add files via upload

---
+++ PhoneBook/Classes/SQLServerConnection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace PhoneBook.Classes
+{
+    class DBClass
+    {
+        public static string GetConnectionStrings()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["conString"].ToString();
+            return connectionString;
+        }
+        public static string sql;
+        public static SqlConnection con = new SqlConnection();
+        public static SqlCommand cmd = new SqlCommand("", con);
+        public static SqlDataReader rd;
+        public static DataTable dt;
+        public static SqlDataAdapter da;
+
+        public static void openConnection()
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.ConnectionString = GetConnectionStrings();
+                    con.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Фэйл. Сорян. " + Environment.NewLine +
+                                "Описание: " + ex.Message.ToString(), "C# WPF Connection To SQLServer",
+                                MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public static void closeConnection()
+        {
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                //
+            }
+        }
+    }
+}
---
+++ PhoneBook/ConnectToSQLServer.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Data;
+using System.Data.SqlClient;
+using PhoneBook.Classes;
+namespace PhoneBook
+{
+    /// <summary>
+    /// Логика взаимодействия для ConnectToSQLServerWindow.xaml
+    /// </summary>
+    public partial class ConnectToSQLServerWindow : Window
+    {
+        public ConnectToSQLServerWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DBClass.openConnection();
+
+            DBClass.sql = "SELECT [CustomerID], [FirstName], [LastName], [Age], [City], [PhoneNumber] FROM Customers;";
+            DBClass.cmd.CommandType = CommandType.Text;
+            DBClass.cmd.CommandText = DBClass.sql;
+
+            DBClass.da = new SqlDataAdapter(DBClass.cmd);
+            DBClass.dt = new DataTable();
+            DBClass.da.Fill(DBClass.dt);
+
+            myDataGrid.ItemsSource = DBClass.dt.DefaultView;
+
+            DBClass.closeConnection();
+        }
+    }
+}
